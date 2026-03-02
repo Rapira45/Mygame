686258f1c9bd6f1bfc5543f0c8456cdb25547992
Rapira45 <lehanovnikita145@gmail.com>
2026-03-02 09:32:59 +0200

    Add files via upload

--- main.gd
+++ main.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+var t = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Label.text = str(t)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	print("tick")
+	t -= 1
+	$Label.text = str(t)
+	if t == 0:
+		$Label.text = ("You lose")
+		$Timer.paused = true
+	
